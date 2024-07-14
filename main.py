eb4b9d0cb3be5b9ca67ef0645c82e76bba745e54
Nazmul Islam <93649312+nazmulislamgit@users.noreply.github.com>
2024-07-14 13:29:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,33 @@ print("Hello, World!")
 
 if 5 > 2:
   print("Five is greater than two!")
+
+
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
